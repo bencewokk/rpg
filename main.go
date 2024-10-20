36fea04af8ac8f834e8a6f5e2dacf31056a400c1
bencewok <bencewok@gmail.com>
2024-10-20 12:56:23 +0200

    .

--- main.go
+++ main.go
@@ -8,19 +8,34 @@ DISCRIPTION
 LOGS
 
 	2024.10.19 ver 0.0.1
-	-This is the beginning of the game. The main game loop is set up, but no game logic has been implemented yet.
-	-Added button ui functions and basic game variables such as gamestate.
-	-Started the menu with options and play buttons
+	- This is the beginning of the game. The main game loop is set up,
+	  but no game logic has been implemented yet.
+	- Added button ui functions and basic game variables such as gamestate.
+	- Started the menu with options and play buttons
+
+	2024.10.20 ver 0.0.2
+	- Moved screenWidth annd screenHeight out of gamestate
+	- Added basic testmap
+	-
 
 TODO
 	important:
-		- add a test map
+		- add a test map  / done
 		- add a test character
-		- decide whether to use rendered map or not
+		- decide whether to use rendered map or not / done
+		- better map creation
 	unimportant:
+		- split up code into individual files
+		- move readme to readme
 */
 
+/*
+for me
+
+ideas: different stats on different tile types
+*/
 import (
+	"fmt"
 	"image/color"
 	"log"
 
@@ -32,17 +47,33 @@ import (
 func gameinit() {
 	ebiten.SetFullscreen(true)
 	ebiten.SetWindowTitle("rpg")
-	width, height := ebiten.Monitor().Size()
-	globalGameState.screenWidth = float32(width)
-	globalGameState.screenHeight = float32(height)
+
 }
 
-// Color variables
+// Color variable for ui
 var (
-	gray        = color.RGBA{128, 128, 128, 255}
-	darkergray  = color.RGBA{100, 100, 100, 255}
-	lightergray = color.RGBA{158, 158, 158, 255}
-	transparent = color.RGBA{0, 0, 0, 0}
+	uigray        = color.RGBA{128, 128, 128, 255}
+	uidarkgray    = color.RGBA{100, 100, 100, 255}
+	uilightgray   = color.RGBA{158, 158, 158, 255}
+	uitransparent = color.RGBA{0, 0, 0, 0}
+)
+
+// Color variable for map rendering
+var (
+	mlightgreen      = color.RGBA{144, 238, 144, 255}
+	mbrown           = color.RGBA{139, 69, 19, 255}
+	mdarkgray        = color.RGBA{169, 169, 169, 255}
+	mdarkgreen       = color.RGBA{34, 139, 34, 255}
+	currenttilecolor = color.RGBA{0, 0, 0, 0}
+)
+
+// Screen sizes
+var (
+	width, height    = ebiten.Monitor().Size()
+	screenWidth      = float32(width)
+	screenHeight     = float32(height)
+	screendivisor    = float32(120)
+	intscreendivisor = 120
 )
 
 // Button struct definition
@@ -57,25 +88,48 @@ type button struct {
 	inactiveColor color.RGBA
 }
 
+var testmap = gamemap{
+	data: [9][16]int{
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		{2, 3, 0, 0, 3, 3, 3, 3, 1, 1, 0, 2, 0, 1, 0, 2},
+		{2, 3, 2, 3, 1, 3, 1, 2, 0, 0, 3, 3, 1, 2, 1, 2},
+		{2, 1, 3, 3, 2, 0, 3, 0, 3, 1, 3, 3, 2, 3, 2, 2},
+		{2, 1, 3, 3, 0, 2, 1, 1, 1, 2, 0, 3, 0, 0, 0, 2},
+		{2, 3, 1, 0, 1, 1, 0, 1, 3, 3, 2, 1, 1, 3, 3, 2},
+		{2, 2, 2, 3, 1, 0, 2, 1, 3, 3, 2, 1, 2, 3, 2, 2},
+		{2, 1, 1, 3, 3, 1, 3, 2, 0, 0, 2, 2, 0, 0, 1, 2},
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+	},
+}
+
+type gamemap struct {
+	// map data (2D array)
+	//
+	//0 plains, 1 hills, 2 mountains, 3 forests
+	data [9][16]int
+}
+
 // contains all global information about the game
 var globalGameState gamestate
 
 // contains all global information about the game
+//
+// contains maps
 type gamestate struct {
 	// 0 menu / 1 menu and options / 3 in game
 	stateid int
 
-	// screen width (x) and height (y)
-	//
-	// defaults are the user's screen dimensions, declared in gameinit
-	//
-	// constant
-	screenWidth, screenHeight float32
-
 	// maps are stored in arrays (see in type map)
 	//
-	// while rendered map array size is constant to 144 (16*9) currentmapid is not
+	//  this is the current map that is  being used//while rendered map array size is constant to 144 (16*9) currentmapid is not
 	currentmapid int
+
+	// character information
+
+	// contains where the character is on the array
+	//
+	//this translates to gamemap[y][x]
+	currentcharposx, currentcharposy int
 }
 
 // Create a new button
@@ -139,30 +193,59 @@ func (g *Game) Update() error {
 // Draw method of the Game
 func (g *Game) Draw(screen *ebiten.Image) {
 
+	fmt.Println(screenHeight, screenWidth)
 	switch globalGameState.stateid {
 	case 0:
 
-		playbtn := createButton("Play", 25, 25, 150, 50, transparent, lightergray, gray)
+		playbtn := createButton("Play", 25, 25, 150, 50, uitransparent, uilightgray, uigray)
 		playbtn.DrawButton(screen)
 		if playbtn.pressed {
 			globalGameState.stateid = 3
 		}
 
-		optionsbtn := createButton("Options", 25, 85, 150, 50, transparent, lightergray, gray)
+		optionsbtn := createButton("Options", 25, 85, 150, 50, uitransparent, uilightgray, uigray)
 		optionsbtn.DrawButton(screen)
 		if optionsbtn.pressed {
 			globalGameState.stateid = 1
 		}
 	case 1:
-		options_exitbtn := createButton("Back to menu", 25, 25, 150, 50, transparent, lightergray, gray)
+		options_exitbtn := createButton("Back to menu", 25, 25, 150, 50, uitransparent, uilightgray, uigray)
 		options_exitbtn.DrawButton(screen)
 		if options_exitbtn.pressed {
 			globalGameState.stateid = 0
 		}
 
-		vector.DrawFilledRect(screen, 200, 25, globalGameState.screenWidth-250, globalGameState.screenHeight-50, darkergray, false)
+		vector.DrawFilledRect(screen, 200, 25, screenWidth-250, screenHeight-50, uidarkgray, false)
 	case 3:
-
+		switch globalGameState.currentmapid {
+		case 0:
+			ebitenutil.DebugPrint(screen, "Test map")
+			// this is 32 because we are rendering that part of the map
+			// one tile is 240x240 px so this would fill the whole screen
+			for i := 0; i < 9; i++ {
+				for j := 0; j < 16; j++ {
+					switch testmap.data[i][j] {
+					case 0:
+						currenttilecolor = mlightgreen
+					case 1:
+						currenttilecolor = mbrown
+					case 2:
+						currenttilecolor = mdarkgray
+					case 3:
+						currenttilecolor = mdarkgreen
+					}
+					vector.DrawFilledRect(
+						screen,
+						float32(j*intscreendivisor),
+						float32(i*intscreendivisor),
+						screendivisor,
+						screendivisor,
+						currenttilecolor,
+						false,
+					)
+				}
+			}
+		}
 	}
 
 }
